c54c062fa480bd346dd8975128d0012c76a8a453
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-11-28 13:48:19 +0000

    SDIT-3227: 🚨 Merge linting configuration from main

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
 export default hmppsConfig({
+  extraIgnorePaths: ['assets'],
   extraPathsAllowingDevDependencies: ['.allowed-scripts.mjs'],
 })
